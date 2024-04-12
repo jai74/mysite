df3d28a8d1c2e2dc4c6f5fb2de9329975b37d284
jai74 <jaibhaarath47@gmail.com>
2024-04-12 20:13:59 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,39 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <html lang="en">
-      <head>
-        <meta charset="utf-8" />
-        <meta name="description" content="Jai Bhaarath's website showcasing projects, skills, and interests in software development and technology." />
-        <link rel="preload" href="InterDisplay-Bold.woff2" as="font" type="font/woff2" crossorigin />
-        <link rel="stylesheet" type="text/css" href="src/app.css" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-        <title>Jai Bhaarath</title>
-      </head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-9JMZVVEVW6"></script>
-      <script>
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-
-        gtag('config', 'G-9JMZVVEVW6');
-        `}
-      </script>
-      <body>
-        <div className="content">
-          <h1 id="my-name">Jai Bhaarath</h1>
-          <a id="my-mail" href="mailto:jb@jaibhaarath.org">jb@jaibhaarath.org</a>
+    <body>
+      <div className="content">
+        <p id="my-name">Jai Bhaarath</p>
+        <a id="my-mail" href="mailto:jb@jaibhaarath.org">jb@jaibhaarath.org</a>
+      </div>
+      <footer>
+        <div className="social-links">
+          <a href="https://www.linkedin.com/in/jai-bhaarath" target="_blank" rel="noopener noreferrer">LinkedIn <span class="arrow">&#x2197;</span></a>
+          <a href="https://www.instagram.com/jaibhaarath" target="_blank" rel="noopener noreferrer">Instagram <span class="arrow">&#x2197;</span></a>
+          <a href="https://www.twitter.com/jaibz" target="_blank" rel="noopener noreferrer">Twitter <span class="arrow">&#x2197;</span></a>
         </div>
-        <footer>
-          <div className="social-links">
-            <a href="https://www.linkedin.com/in/jai-bhaarath" target="_blank" rel="noopener noreferrer">LinkedIn <span class="arrow">&#x2197;</span></a>
-            <a href="https://www.instagram.com/jaibhaarath" target="_blank" rel="noopener noreferrer">Instagram <span class="arrow">&#x2197;</span></a>
-            <a href="https://www.twitter.com/jaibz" target="_blank" rel="noopener noreferrer">Twitter <span class="arrow">&#x2197;</span></a>
-          </div>
-        </footer>
-      </body>
-    </html>
+      </footer>
+    </body>
   );
 }
 
